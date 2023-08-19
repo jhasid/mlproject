d879e6495cd50a114270a52bc3d52ef7af30fc0f
unknown <siddharth.jha30@gmail.com>
2023-08-19 18:50:41 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -23,4 +23,4 @@ class CustomException(Exception):
 #        a=1/0
 #    except Exception as e:
 #        logging.info("Divide by zero")
-#        raise CustomException(e,sys)
+3        raise CustomException(e,sys)
